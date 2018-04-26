ef78dadbc311c1519d19faa016932fffb47361b9
Andrei Silva <andrei-a-c-s@hotmail.com>
2018-04-26 14:53:42 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/andrei/modelagemconceitual/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.andrei.modelagemconceitual.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andrei.modelagemconceitual.domain.Cliente;
+import com.andrei.modelagemconceitual.repositories.ClienteRepository;
+import com.andrei.modelagemconceitual.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
