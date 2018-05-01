b39fc503822ce1d0432877f21f5f7e92c56a65d2
Andrei Silva <andrei-a-c-s@hotmail.com>
2018-05-01 11:51:54 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/andrei/modelagemconceitual/services/CategoriaService.java
+++ src/main/java/com/andrei/modelagemconceitual/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.andrei.modelagemconceitual.domain.Categoria;
+import com.andrei.modelagemconceitual.dto.CategoriaDTO;
 import com.andrei.modelagemconceitual.repositories.CategoriaRepository;
 import com.andrei.modelagemconceitual.services.exceptions.DataIntegrityException;
 import com.andrei.modelagemconceitual.services.exceptions.ObjectNotFoundException;
@@ -52,5 +53,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
