7a9b420284fea7252b040a9e275b0936458c51c9
Andrei Silva <andrei-a-c-s@hotmail.com>
2018-05-03 15:57:38 -0300

    Validacao customizada: email repetido

--- src/main/java/com/andrei/modelagemconceitual/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/andrei/modelagemconceitual/services/validation/ClienteInsertValidator.java
@@ -1,15 +1,23 @@
 package com.andrei.modelagemconceitual.services.validation;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.andrei.modelagemconceitual.domain.Cliente;
 import com.andrei.modelagemconceitual.domain.enums.TipoCliente;
 import com.andrei.modelagemconceitual.dto.ClienteNewDTO;
+import com.andrei.modelagemconceitual.repositories.ClienteRepository;
 import com.andrei.modelagemconceitual.resources.exception.FieldMessage;
 import com.andrei.modelagemconceitual.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDTO.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
