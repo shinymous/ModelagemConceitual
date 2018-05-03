9ddcd11ecdb15b4c88469f5f2a3595c4219ebc1c
Andrei Silva <andrei-a-c-s@hotmail.com>
2018-05-03 17:42:57 -0300

    Consulta de Produto por nomes e categorias

--- src/main/java/com/andrei/modelagemconceitual/repositories/ProdutoRepository.java
+++ src/main/java/com/andrei/modelagemconceitual/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.andrei.modelagemconceitual.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.andrei.modelagemconceitual.domain.Categoria;
 import com.andrei.modelagemconceitual.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+	
+	// Query equivalente
+	// @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias") 
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
--- src/main/java/com/andrei/modelagemconceitual/services/validation/ClienteInsert.java
+++ src/main/java/com/andrei/modelagemconceitual/services/validation/ClienteInsert.java
@@ -15,7 +15,7 @@ import javax.validation.Payload;
 public @interface ClienteInsert {
 	String message() default "Erro de validação";
 
-	Class<?>[] groups() default {};
+	Class<?>[] groups() default {}; 
 
 	Class<? extends Payload>[] payload() default {};
 }
