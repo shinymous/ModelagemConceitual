8ecb224dea310bed396cb03c82789fe9257471ab
Andrei Silva <andrei-a-c-s@hotmail.com>
2018-05-07 15:20:23 -0300

    Criando profile de teste

--- src/main/java/com/andrei/modelagemconceitual/resources/PedidoResource.java
+++ src/main/java/com/andrei/modelagemconceitual/resources/PedidoResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.andrei.modelagemconceitual.domain.Cliente;
 import com.andrei.modelagemconceitual.domain.Pedido;
-import com.andrei.modelagemconceitual.dto.ClienteNewDTO;
 import com.andrei.modelagemconceitual.services.PedidoService;
 
 @RestController
