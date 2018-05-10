6d3b9f36713c19cb5c9e8cdd6c69449417224c18
Andrei Silva <andrei-a-c-s@hotmail.com>
2018-05-10 14:36:51 -0300

    Implementando SmtpEmailService com servidor Google

--- src/main/java/com/andrei/modelagemconceitual/config/DevConfig.java
+++ src/main/java/com/andrei/modelagemconceitual/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.andrei.modelagemconceitual.services.DBService;
+import com.andrei.modelagemconceitual.services.EmailService;
+import com.andrei.modelagemconceitual.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instantiateDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
--- src/main/java/com/andrei/modelagemconceitual/services/DBService.java
+++ src/main/java/com/andrei/modelagemconceitual/services/DBService.java
@@ -113,7 +113,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com","1234", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "andrei-a-c-s@hotmail.com","1234", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("123123123123","12312312312"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "1231234423", cli1, c1);
