7ab40f523384da800a9ab0fc60e6b3ef1076b622
Andrei Silva <andrei-a-c-s@hotmail.com>
2018-05-14 14:05:57 -0300

    Adicionando senha a cliente

--- src/main/java/com/andrei/modelagemconceitual/services/DBService.java
+++ src/main/java/com/andrei/modelagemconceitual/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.andrei.modelagemconceitual.domain.Categoria;
@@ -33,6 +34,8 @@ import com.andrei.modelagemconceitual.repositories.ProdutoRepository;
 @Service
 public class DBService {
 
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	@Autowired
 	CategoriaRepository categoriaRepository;
 	@Autowired
@@ -113,7 +116,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "andrei-a-c-s@hotmail.com","1234", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "andrei-a-c-s@hotmail.com","1234", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("123123123123","12312312312"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "1231234423", cli1, c1);
