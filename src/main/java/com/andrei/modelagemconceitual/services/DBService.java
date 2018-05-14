e67c83a0204be3d5e08a25f1f5eb7f382c305b69
Andrei Silva <andrei-a-c-s@hotmail.com>
2018-05-14 16:51:00 -0300

    Salvando perfis de usuario na base da dados

--- src/main/java/com/andrei/modelagemconceitual/services/DBService.java
+++ src/main/java/com/andrei/modelagemconceitual/services/DBService.java
@@ -20,6 +20,7 @@ import com.andrei.modelagemconceitual.domain.PagamentoComCartao;
 import com.andrei.modelagemconceitual.domain.Pedido;
 import com.andrei.modelagemconceitual.domain.Produto;
 import com.andrei.modelagemconceitual.domain.enums.EstadoPagamento;
+import com.andrei.modelagemconceitual.domain.enums.Perfil;
 import com.andrei.modelagemconceitual.domain.enums.TipoCliente;
 import com.andrei.modelagemconceitual.repositories.CategoriaRepository;
 import com.andrei.modelagemconceitual.repositories.CidadeRepository;
@@ -119,13 +120,18 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "andrei-a-c-s@hotmail.com","1234", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("123123123123","12312312312"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "andreicamargo97@gmail.com","78452939035", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("123123123121", "12312312315"));
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "1231234423", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
-		
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "47877012", cli2, c2);
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e1, e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		clienteRepository.saveAll(Arrays.asList(cli1,cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2,e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
