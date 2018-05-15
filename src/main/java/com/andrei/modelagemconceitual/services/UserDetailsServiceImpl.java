dbf921de33e82e410cd85c44803a9a24aea3ec13
Andrei Silva <andrei-a-c-s@hotmail.com>
2018-05-15 16:37:04 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/andrei/modelagemconceitual/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.andrei.modelagemconceitual.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.andrei.modelagemconceitual.domain.Cliente;
+import com.andrei.modelagemconceitual.repositories.ClienteRepository;
+import com.andrei.modelagemconceitual.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
